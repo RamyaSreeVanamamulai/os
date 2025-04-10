b7e11a5d8cfc1937f0020ce4a769779beddb105c
RamyaSreeVanamamulai <165733500+RamyaSreeVanamamulai@users.noreply.github.com>
2025-04-10 10:51:15 -0400

    Create showargs.c

---
+++ showargs.c
@@ -0,0 +1,22 @@
+/* This program demonstrates command line argument handling */
+#include <stdio.h>
+
+void showargs(int, char **);
+
+#define STAND_ALONE 1
+
+#ifdef STAND_ALONE
+int main(int argc, char *argv[]) {
+    printf("Written by your name\n");
+    showargs(argc, argv);
+    return (0);
+}
+#endif
+
+void showargs(int argc, char *argv[]) {
+    int i;
+    printf("Program name: %s\n", argv[0]);
+    printf("There are %d arguments\n", argc);
+    for (i=1; i<argc; i++)
+        printf("%2d. %s\n", i, argv[i]);
+}
